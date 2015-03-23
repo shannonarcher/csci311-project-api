1b0fa1e08a691e58bee40858c3e7687e88942141
Shannon Archer <shannon.archer0@gmail.com>
2015-03-23 21:43:27 +1100

    completed api 0.2.0 migrations

--- database/migrations/2015_03_23_102255_create_task_comments_table.php
+++ database/migrations/2015_03_23_102255_create_task_comments_table.php
@@ -13,6 +13,19 @@ class CreateTaskCommentsTable extends Migration {
 	public function up()
 	{
 		//
+		Schema::create('task_comments', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('comment', 400);
+			$table->timestamp('archived_at')->nullable();
+
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+
+			$table->integer('task_id')->unsigned();
+			$table->foreign('task_id')->references('id')->on('tasks');
+
+			$table->timestamps();
+		});
 	}
 
 	/**
@@ -23,6 +36,7 @@ class CreateTaskCommentsTable extends Migration {
 	public function down()
 	{
 		//
+		Schema::drop('task_comments');
 	}
 
 }
